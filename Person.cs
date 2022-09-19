5a5669d46653ed858fb7ab886430bdf86f0e3ae2
ArianaTrinidad <quirino.ariana1@gmail.com>
2022-09-19 19:37:56 -0300

    Agregar archivos de proyecto.

---
+++ Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garbage_Collector
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public Person ChildOne { get; set; }
+        public Person ChildTwo { get; set; }
+
+        ~Person() //finalizador: metodo que se llama cuando un objeto del GC se recoge, se usan para rastrear
+        {
+            Console.WriteLine($"   Collecting {Name}.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Garbage_Collector
+{
+    class Program
+    {
+        static void ShortLives (Person parent)
+        {
+            Person fred = new Person
+            {
+                Name = "Fred",
+                ChildOne = new Person { Name = "Bamm-Bamm"}
+            };
+            parent.ChildTwo = fred.ChildOne;
+        }
+        static void Run( )
+        {
+            Person wilma = new Person
+            {
+                Name = "Wilma",
+                ChildOne = new Person { Name = "Pebbles" }
+            };
+            ShortLives(wilma);
+
+            Console.WriteLine("Leaving 'ShortLives'...");
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+        static void Main()
+        {
+            Run();
+
+            Console.WriteLine("\nLeaving  'Run'...");
+
+            GC.Collect();// lo ideal es no hacerlo, el  GC sabe cuando es apropiado realizar una recolleción
+            GC.WaitForPendingFinalizers(); // el programa terminará cuando llamen a los finalizadores 
+
+            Console.ReadLine();
+        }
+
+        //ShortLives es usado para hacer referencia de bambam a wilma y declarar que tambien es hijo de ella
+        //Una vez que bambam es asignado cómo hijo2 de Wilma se disposea la memoria que uasaba para fred
+        //Una vez finalizada la coneccion entre hijos y pades
+        //Disposea a BammBamm, Pebbles y Wilma
+    }
+}
